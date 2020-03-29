22938000023e16ea693fcfec22dfc6c68ef65ba0
xinhe0616 <648652898@qq.com>
2020-03-29 12:10:26 +0800

    3/29

---
+++ src/main/java/com/xinhe/HellomavenApplication.java
@@ -0,0 +1,13 @@
+package com.xinhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellomavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinhe/HellomavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
