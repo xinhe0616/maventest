473402783d2fb53f0240cbf2af11bebba6d1093c
xinhe0616 <648652898@qq.com>
2020-03-29 12:16:37 +0800

    3/29

--- src/main/java/com/xinhe/controller/MyController.java
+++ src/main/java/com/xinhe/controller/MyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-    @RequestMapping("/")
+    @RequestMapping("/hellomaven-0.0.1-SNAPSHOT")
     public String hello(){
         return "hello maven!";
     }
